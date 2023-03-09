9c01e0ed232045b189f27b82c9318855cd39b9c8
Tony Phillips <anthonyp2403@gmail.com>
2023-03-09 17:08:00 +0000

    made colour change and text amendment

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
       <div className='colour-border'>
         <VatRateField customstyle="field" vatRateChanged={handleVatRateChanged} value={vatRate} updatePrices={updatePrices} />
         <PriceEntryField customstyle="field" label="Price excl VAT: " priceChanged={handleNetPriceChange} price={netPrice === 0.0 ? "" : netPrice} />
-        <DisplayBlock customstyle="field" label="VAT to pay: " value={vatToPay} />
+        <DisplayBlock customstyle="field" label="Incorrect VAT to pay: " value={vatToPay} />
         <PriceEntryField customstyle="field" label="Price incl VAT: " priceChanged={handleGrossPriceChange} price={grossPrice === 0.0 ? "" : grossPrice} />
       </div>
     </div>
